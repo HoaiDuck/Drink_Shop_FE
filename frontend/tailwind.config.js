c95b453ecb161004d78f936a756950daf7cf28d3
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-12-10 21:25:08 +0700

    thêm thuộc tính jst vào tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        fontjapan: ['"Noto Sans Japanese"', 'sans-serif']
-      }
+        fontjapan: ['"Noto Sans Japanese"', "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
