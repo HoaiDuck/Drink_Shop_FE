0aba93aa2eb91ac5d30e8925e6737a1e03f9656f
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-12-12 16:10:35 +0700

    co the tip tuc lam

--- src/components/Layout/Nav/BtnHome.jsx
+++ src/components/Layout/Nav/BtnHome.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
-const BtnHome = () =>{
-    return (
-        <Link to="/Home/">
-            <button className="font-medium px-4 py-2 bg-gray-300 rounded-full hover:bg-gray-300 hover:text-black">Trang chủ</button>
-        </Link>
-    )
-}
-export default BtnHome
+const BtnHome = () => {
+  return (
+    <Link to="/">
+      <button className="font-medium px-4 py-2 bg-gray-300 rounded-full hover:bg-gray-300 hover:text-black">
+        Trang chủ
+      </button>
+    </Link>
+  );
+};
+export default BtnHome;
