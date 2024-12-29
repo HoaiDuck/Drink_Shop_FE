138f47a8b0e85a9582690f1b43f854518aaadb0e
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-12-30 00:24:34 +0700

    đã fix xong bug double click

--- src/components/Layout/PrivateRouter.jsx
+++ src/components/Layout/PrivateRouter.jsx
@@ -1,20 +1,29 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAbility } from "@casl/react";
 
 import { AuthContext } from "@/context/AuthContext";
 import { ability, defineAbilitiesFor } from "@/ability";
+
 function PrivateRoute({ action, subject, children }) {
   const { user, setUser } = useContext(AuthContext);
-  useEffect(() => {
-    console.log(">>>CHECK context User:", user);
-    const roleNumber = Number(user.userRole); // Đảm bảo role là number
-    const permissions = defineAbilitiesFor(roleNumber);
-    ability.update(permissions); // Cập nhật Ability
-    console.log("Updated abilities:", ability);
-  }, []);
+  const [loading, setLoading] = useState(true);
 
-  console.log("Ability:", ability); // Kiểm tra xem ability có giá trị hay không
+  useEffect(() => {
+    if (user) {
+      console.log(">>>CHECK context User:", user);
+      const roleNumber = Number(user.userRole); // Đảm bảo role là number
+      const permissions = defineAbilitiesFor(roleNumber);
+      ability.update(permissions); // Cập nhật Ability
+      setLoading(false); // Đánh dấu rằng quá trình cập nhật đã hoàn tất
+      console.log("Updated abilities:", ability);
+    }
+  }, [user]);
+
+  if (loading) {
+    return null; //nếu ability chưa xong, sẽ ép chạy lại
+  }
+
   if (!ability.can(action, subject)) {
     return <Navigate to="/unauthorized" />;
   }
