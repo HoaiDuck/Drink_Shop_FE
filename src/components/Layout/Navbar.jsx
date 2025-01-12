280acb50ab30bb51bd35b1645eaa2bfce2082ac4
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2025-01-12 14:10:48 +0700

    đã deploy được FE

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -137,9 +137,32 @@ const Navbar = ({ cateData }) => {
                 </button>
               </Link>
             </CheckRole>
-            <Link to="/Cart">
+            <CheckRole action="purchase" subject="Item">
+              <Link to="/Cart">
+                <button className="flex flex-row px-4 py-2 bg-gray-300 rounded-full font-medium hover:bg-gray-400">
+                  Cart
+                  <span className="ml-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                      />
+                    </svg>
+                  </span>
+                </button>
+              </Link>
+            </CheckRole>
+            <Link to="/Login">
               <button className="flex flex-row px-4 py-2 bg-gray-300 rounded-full font-medium hover:bg-gray-400">
-                Cart
+                Login
                 <span className="ml-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
